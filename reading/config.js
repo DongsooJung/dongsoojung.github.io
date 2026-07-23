27111c4b395ee29c996a0af3e13c549e05029438
Dongsoo Jung <100942621+DongsooJung@users.noreply.github.com>
2026-07-23 15:01:06 +0900

    Add reading Supabase configuration

---
+++ reading/config.js
@@ -0,0 +1,5 @@
+window.STARGATE_READING_CONFIG = {
+  supabaseUrl: "https://inftexpcnfinglwlrvsj.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZnRleHBjbmZpbmdsd2xydnNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTMyMzgsImV4cCI6MjA4ODQ4OTIzOH0.HONuULp0L3B5T0gTiwJMnowjJonJzzNHhUV_LtpDQoI",
+  table: "reading_posts"
+};
